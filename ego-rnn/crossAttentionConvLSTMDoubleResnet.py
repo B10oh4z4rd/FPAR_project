cdfa34f53fa9c8850eb5fc9bb72af342322a7b8a
erichMalan <erich.malan@gmail.com>
2020-08-13 14:24:03 +0200

    cross attention

--- ego-rnn/crossAttentionConvLSTMDoubleResnet.py
+++ ego-rnn/crossAttentionConvLSTMDoubleResnet.py
@@ -8,7 +8,7 @@ from objectAttentionModelConvLSTM import *
 from noAttentionConvLSTM import *
 
 
-class attentionDoubleResnet(nn.Module):
+class crossAttentionDoubleResnet(nn.Module):
     def __init__(self, num_classes=61, mem_size=512, rgbm = None, fcm = None):
         super(attentionDoubleResnet, self).__init__()
         self.num_classes = num_classes
@@ -29,15 +29,12 @@ class attentionDoubleResnet(nn.Module):
         
         self.avgpool = nn.AvgPool2d(7)
         self.dropout = nn.Dropout(0.7)
-        self.fc = nn.Linear(2 * mem_size, self.num_classes)
+        self.fc = nn.Linear(mem_size, self.num_classes)
         self.classifier = nn.Sequential(self.dropout, self.fc)
 
     def forward(self, inputVariable, inputVariable2):
         state_x = (Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cuda()),
                  Variable(torch.zeros((inputVariable.size(1), self.mem_size, 7, 7)).cuda()))
-        state_y = (Variable(torch.zeros((inputVariable2.size(1), self.mem_size, 7, 7)).cuda()),
-                 Variable(torch.zeros((inputVariable2.size(1), self.mem_size, 7, 7)).cuda()))
-        
         for t in range(inputVariable.size(0)):
             logit, feature_conv, feature_convNBN = self.resNet1(inputVariable[t])
             logit2, feature_conv2, feature_convNBN2 = self.resNet2(inputVariable2[t])
@@ -51,8 +48,6 @@ class attentionDoubleResnet(nn.Module):
             attentionFeat = feature_convNBN2 * attentionMAP.expand_as(feature_conv)
             state_x = self.lstm_cell_x(attentionFeat, state_x)
             
-        feats1 = self.avgpool(state_x[1]).view(state_x[1].size(0), -1)
-        feats2 = self.avgpool(state_y[1]).view(state_y[1].size(0), -1)
-        feats_xy = torch.cat((feats1,feats2), 1)
-        feats = self.classifier(feats_xy)
-        return feats, feats_xy
+        feats = self.avgpool(state_x[1]).view(state_x[1].size(0), -1)
+        feats = self.classifier(feats)
+        return feats
